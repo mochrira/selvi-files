c04f3057e2597289ba7354395fee21638b318781
Moch Rizal Rachmadani <mochrira@gmail.com>
2020-10-23 07:38:38 +0700

    file upload done

--- src/FileController.php
+++ src/FileController.php
@@ -7,29 +7,45 @@ use Selvi\Route;
 
 class FileController extends Controller {
 
-    public static $path;
+    public static $basePath;
 
+    /**
+     * Setup API to handle file upload
+     * @param ArrayObject $config
+     */
     public static function setup($config = []) {
-        if(isset($config['path'])) {
-            self::$path = $config['path'];
+        if(isset($config['basePath'])) {
+            self::$basePath = $config['basePath'];
         }
         Route::post('/files', '\\Selvi\\Files\\FileController@upload');
         Route::get('/files/(:any)', '\\Selvi\\Files\\FileController@download');
     }
     
+    /**
+     * POST files
+     * 
+     * Request Body (multipart/form-data)
+     * File : File to upload
+     */
     function upload() {
         $file = $this->input->file('file');
-        if(!is_dir(self::$path)) {
-            mkdir(self::$path);
+        if(!is_dir(self::$basePath)) {
+            mkdir(self::$basePath);
         }
-        move_uploaded_file($file['tmp_name'], self::$path.'/'.$file['name']);
+        move_uploaded_file($file['tmp_name'], self::$basePath.'/'.$file['name']);
         return jsonResponse([
             'fileUrl' => base_url().'files/'.$file['name']
         ]);
     }
 
+    /**
+     * GET files/{filePath}
+     * 
+     * URI Parameters
+     * filePath : path to file relative to basepath
+     */
     function download() {
-        $file = self::$path.str_replace('/files', '', urldecode($this->uri->getUri()));
+        $file = self::$basePath.str_replace('/files', '', urldecode($this->uri->getUri()));
         if(!is_file($file)) {
             return response('', 404);
         }
